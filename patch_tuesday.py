80d8b2b439e6a11d43f4dacea1e4ed837373841b
Rob Langford <rob.langford1@gmail.com>
2022-02-03 23:22:28 +1000

    notes on initial idea to calculate patch tuesday

---
+++ patch_tuesday.py
@@ -0,0 +1,8 @@
+# Simple script to calculate Patch Tuesday
+
+# Inputs: Year [Format: YY or YYYY], Month [Format: MM or Month abbrieviation Jan, Feb, etc. or Full Month March, July, etc.)
+# Outputs: date of Microsoft Patch Tuesday [Format: DD/MM/YYYY]
+
+# Initial idea is to iterate through month to determine Tuesdays and return the second Tuesday 
+
+# python3 library module calendar - https://docs.python.org/3/library/calendar.html
