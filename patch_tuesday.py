2e5258468f9df28c7284b7c9a6b1c03d7dc138d6
Rob Langford <rob.langford1@gmail.com>
2022-02-20 18:52:31 +1000

    Added Docstrings and error handling on input validation

--- patch_tuesday.py
+++ patch_tuesday.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
+"""Simple script to calculate Microsofts Patch Tuesday (2nd Tuesday of the month)
 
-# Simple script to calculate Patch Tuesday
+Usage: 
 
-# Inputs: Year [Format: YY or YYYY], Month [Format: MM or Month abbrieviation Jan, Feb, etc. or Full Month March, July, etc.)
-# Outputs: date of Microsoft Patch Tuesday [Format: DD/MM/YYYY]
+    python3 patch_tuesday.py <MONTH> <YEAR>
 
-''' 
+Args:
+    month: The Month in a numeric format: e.g. 1 = Jan, 2 Feb etc.
+    year: The year in a numberic format: e.g. 2022, 2023
+
+
 Depending on the day of the 1st of the month there is an offset:
 Wed = +13days
 Thu = +12days
@@ -14,34 +18,32 @@ Sat = +10days
 Sun = +9days
 Mon = +8days
 Tue = +7days
-
-'''
-
+"""
+# Future enhancements: 
 # python3 library module calendar - https://docs.python.org/3/library/calendar.html
 
 # Libraries:
 import calendar
 from datetime import date
 
-def patch_tuesday(input_month, input_year):
 
-    # import calendar
-    # from datetime import date
+def patch_tuesday(month, year):
+    """Calculate the Microsoft Patch tuesday for a given month or year
 
+    Args:
+        month: The Month in a numeric format: e.g. 1 = Jan, 2 Feb etc.
+        year: The year in a numberic format: e.g. 2022, 2023
+    """
     # Defined dictionary for the offset days for Patch Tuesdays
     tuesday_offset = {'Monday':9, 'Tuesday':8, 'Wednesday':14, 'Thursday':13, 'Friday':12, 'Saturday':11, 'Sunday':10}
-
-    # Input year and month
-    # input_year = input("Please input year: ")
-    # input_month = input("Please input month: ")
 
     # Calculate First Day of the Month
     # calendar.weekday returns the numeric value based on the integer inputs of year, month, day
     # calendar.day_name returns the name of the day based on a numeric input: 0 = Monday, 1 = Tuesday, etc.
-    first_day = calendar.day_name[calendar.weekday(int(input_year),int(input_month),1)]
+    first_day = calendar.day_name[calendar.weekday(int(year),int(month),1)]
 
     # Concatenate to details for date.
-    patch_tuesday_date = str(tuesday_offset[first_day])+"/"+input_month+"/"+input_year
+    patch_tuesday_date = str(tuesday_offset[first_day])+"/"+str(month)+"/"+str(year)
 
     # Store just the Patch Tuesday date
     patch_tuesday = tuesday_offset[first_day]
@@ -53,6 +55,12 @@ def patch_tuesday(input_month, input_year):
 
 # Main program:
 def main():
+    """Print the day and date of patch Tuesday
+
+    Args:
+        month: The Month in a numeric format: e.g. 1 = Jan, 2 Feb etc.
+        year: The year in a numberic format: e.g. 2022, 2023
+    """
 
     #Input year and month
     input_year = input("Please input year: ")
@@ -61,6 +69,12 @@ def main():
     dates = patch_tuesday(input_month,input_year)
 
     # print out the details.
-    print("Patch Tuesday for ", calendar.month_name[int(input_month)], "-",input_year, " = ", dates['patch_tuesday_date'])
+    print("Patch Tuesday for ", calendar.month_name[int(input_month)], "-",int(input_year), " = ", dates['patch_tuesday_date'])
 
-main()
+
+if __name__ == '__main__':
+    try:
+        arg = sys.argv[1]
+    except IndexError:
+        raise SystemExit(f"Usage: {sys.argv[0]} <month> <year>")
+    main()
